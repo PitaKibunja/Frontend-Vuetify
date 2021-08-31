06d73ffcbcc046084ba5af365309761f5cd08517
dapintechnologies <dapintech@gmail.com>
2021-08-31 06:24:16 +0300

    Start on popups

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,12 +7,35 @@
                 <span>Ninja</span>
             </v-toolbar-title>
             <v-spacer></v-spacer>
+            <v-menu offset-y>
+                <template v-slot:activator="{on,attrs}">
+                     <v-btn flat dark color="grey" v-bind="attrs" v-on="on">
+                       <v-icon>mdi-chevron-down</v-icon>
+                       <span>Menu</span>  
+                     </v-btn>
+                </template>
+               <v-list>
+                   <v-list-item v-for="link in links" :key="link.text">
+                       <router-link :to="link.route">
+                           <v-list-item-title>{{ link.text }}</v-list-item-title>
+                       </router-link>
+                   </v-list-item>
+               </v-list>
+            </v-menu>
             <v-btn flat color="grey">
                 <span>Sign Out</span>
                 <v-icon right>mdi-exit-to-app</v-icon>
             </v-btn>
         </v-toolbar>
         <v-navigation-drawer v-model="drawer" app class="success">
+          <v-layout column align-center>
+              <v-flex class="mt-5">
+                  <v-avatar size="100">
+                      <img src="../images/2pac.jpg" alt="">
+                  </v-avatar>
+                  <p class="white--text subheading mt-1">2 Pac Shakur</p>
+              </v-flex>
+          </v-layout>
            <v-list>
                <v-list-tile v-for="link in links" :key="link.text">
                    <router-link :to="link.route">
--- src/views/Teams.vue
+++ src/views/Teams.vue
@@ -2,7 +2,30 @@
   <div class="teams">
     <h1 class="subheading grey--text">Teams</h1>
     <v-container class="my-5">
-    content
+    <v-layout>
+      <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
+        <v-card elevation="2" outlined shaped tile flat class="text-xs-center ma-3">
+          <v-responsive class="pt-4">
+            <!-- <v-avatar size="100" class="grey lighten-2">
+              <v-icon>{{ person.avatar }}</v-icon>
+            </v-avatar> -->
+              <v-avatar size="100">
+                      <img src="../images/2pac.jpg" alt="">
+              </v-avatar>
+          </v-responsive>
+          <v-card-text>
+            <div class="subheading">{{ person.name }}</div>
+            <div class="grey--text">{{ person.role }}</div>
+          </v-card-text>
+          <v-card-action>
+            <v-btn flat color="grey">
+              <v-icon small left>mdi-message</v-icon>
+            </v-btn>
+            <span>Message</span>
+          </v-card-action>
+        </v-card>
+      </v-flex>
+    </v-layout>
     </v-container>
   </div>
 </template>
@@ -11,6 +34,17 @@
 
   export default {
     name: 'Teams',
+    data(){
+      return{
+        team: [
+        { name: 'The Net Ninja', role: 'Web developer',avatar:'../images/2pac.jpg' },
+        { name: 'Ryu', role: 'Graphic designer' ,avatar:'mdi-account-check-outline'},
+        { name: 'Chun Li', role: 'Web developer' ,avatar:'mdi-account-multiple-check'},
+        { name: 'Gouken', role: 'Social media maverick' ,avatar:'mdi-account-plus-outline'},
+        { name: 'Yoshi', role: 'Sales guru',avatar:'mdi-account-voice'}
+      ]
+      }
+    }
 
   }
 </script>
